94f0265414ddc51bfd19020cc9578984e85f7575
Madzhun Khakim <hakimmadgun@gmail.com>
2023-05-15 01:11:26 +0600

    Now everything is work)) Add FrontEnd code for Pagination add Test Data to DataBase and Create new Migration for it

---
+++ src/main/resources/db/migration/V03__03__insert__test__data__to__table.sql
@@ -0,0 +1,10 @@
+INSERT INTO products (name, description, price, image)
+VALUES
+('Test1', 'TestD1', 100.99, 'Test.jpg'),
+('Test2', 'TestD2', 200.99, 'Test.jpg'),
+('Test3', 'TestD3', 300.99, 'Test.jpg'),
+('Test4', 'TestD4', 400.99, 'Test.jpg'),
+('Test5', 'TestD5', 500.99, 'Test.jpg'),
+('Test6', 'TestD6', 600.99, 'Test.jpg'),
+('Test7', 'TestD7', 700.99, 'Test.jpg'),
+('Test8', 'TestD8', 800.99, 'Test.jpg')
--- src/main/resources/static/script.js
+++ src/main/resources/static/script.js
@@ -18,25 +18,79 @@ function displayProducts(products) {
   });
 }
 
-function hideProductList() {
-  const productDiv = document.getElementById("product-list");
-  productDiv.innerHTML = "";
+function displayPagination(page) {
+
+  const pagination = document.getElementById("pagination");
+  pagination.innerHTML = "";
+
+  const pageNumbers = [];
+  const totalPages = page.totalPages;
+  const currentPage = page.number + 1;
+
+  let startPage = 1;
+  let endPage = totalPages;
+
+  const maxPages = 5;
+  if (totalPages > maxPages) {
+    const middle = Math.floor(maxPages / 2);
+    if (currentPage <= middle) {
+      endPage = maxPages;
+    } else if (currentPage + middle >= totalPages) {
+      startPage = totalPages - maxPages + 1;
+    } else {
+      startPage = currentPage - middle;
+      endPage = currentPage + middle;
+    }
+  }
+
+  if (startPage > 1) {
+    const li = document.createElement("li");
+    const link = document.createElement("a");
+    link.href = "#";
+    link.innerHTML = "1";
+    li.appendChild(link);
+    pagination.appendChild(li);
+    pagination.appendChild(document.createTextNode("..."));
+  }
+
+  for (let i = startPage; i <= endPage; i++) {
+    const li = document.createElement("li");
+    const link = document.createElement("a");
+    link.href = "#";
+    link.innerHTML = i;
+    if (i === currentPage) {
+      li.classList.add("active");
+    }
+    li.appendChild(link);
+    pagination.appendChild(li);
+  }
+
+  if (endPage < totalPages) {
+    pagination.appendChild(document.createTextNode("..."));
+    const li = document.createElement("li");
+    const link = document.createElement("a");
+    link.href = "#";
+    link.innerHTML = totalPages;
+    li.appendChild(link);
+    pagination.appendChild(li);
+  }
+
+  const links = pagination.getElementsByTagName("a");
+  for (let i = 0; i < links.length; i++) {
+    const link = links[i];
+    link.addEventListener("click", async (e) => {
+      e.preventDefault();
+      const pageNumber = parseInt(link.innerHTML) - 1;
+      const response = await fetch(`/product?page=${pageNumber}&size=10`);
+      const products = await response.json();
+      displayProducts(products.content);
+      displayPagination(products);
+    });
+  }
 }
-const searchForm = document.getElementById("search-form");
-searchForm.addEventListener("submit", async (e) => {
-  e.preventDefault();
-  const formData = new FormData(searchForm);
-  const searchParams = new URLSearchParams(formData).toString();
-  const response = await fetch(`/product/search?${searchParams}`);
-  const products = await response.json();
-  hideProductList();
-
-  displayProducts(products.content);
-});
-
-
 window.onload = async () => {
   const response = await fetch("/product");
   const products = await response.json();
   displayProducts(products.content);
+  displayPagination(products);
 };
